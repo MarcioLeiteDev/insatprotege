723eadf03ae1bc4c2b5755ee01062c8d3f5dff42
Marcio Leite <marcio.leite@capitalconsig.local>
2024-07-15 19:18:54 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insat rastreadores Via Satelite</title>
+    <link rel="stylesheet" href="./css/app.css">
+    <script src="https://kit.fontawesome.com/aab7d282e7.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+ 
+</head>
+<body>
+<heaader>
+    <nav class="navbar navbar-expand-lg bg-warning fixed-top">
+        <div class="container-fluid" id="menu-superior">
+          
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-center mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#section1"><i class="fa-solid fa-house"></i>  Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#section2"> <i class="fa-solid fa-users"></i>  Quem Somos</a>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#section1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa-solid fa-bag-shopping"></i>  Produtos
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#section3"> <i class="fa-solid fa-thumbtack"></i>  Rastreador para Veículos</a></li>
+                  <li><a class="dropdown-item" href="#section4"><i class="fa-solid fa-thumbtack"></i>  Rastreador para Motos</a></li>
+                  <li><a class="dropdown-item" href="#section5"><i class="fa-solid fa-thumbtack"></i>  Rastreador para Utilitários</a></li>
+                  <li><a class="dropdown-item" href="#section6"><i class="fa-solid fa-thumbtack"></i>  Rastreador para Caminhão</a></li>
+                  <li><a class="dropdown-item" href="#section7"><i class="fa-solid fa-thumbtack"></i>  Rastreador para Frotas</a></li>
+                
+                </ul>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#section8"><i class="fa-solid fa-universal-access"></i>  Seja Representante</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#section9"><i class="fa-solid fa-envelope"></i>  Contato</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#section10" ><button class="botao-office"><i class="fa-solid fa-globe"></i>Escritorio Virtual</button></a>
+              </li>
+    
+              
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+    </heaader>
+
+    <main>
+    <section class="section1" id="section1" data-aos="fade-up">
+        <div class="box" data-aos="fade-right">
+           <h1>Rastreadores INSAT</h1> 
+           <blockquote>
+            <p>Rastreadores via satélite para veículos, motos, utilitários, caminhões, máquinas agrícolas, alta tecnologia via satélite.</p>
+
+            <p>Conheça nossas melhores opções para segurança pessoal, veicular, para frotas e cargas, com rastreadores altamente tecnológicos.</p>
+
+            <p>A Insat , empresa de rastreamento via satélite sempre estará ao seu lado, protegendo você, sua família e seu patrimônio.</p>
+
+            <p>Nossa meta é oferecer o que há de melhor em soluções, produtos e serviços, para atender e superar às suas expectativas. Este é o nosso compromisso e, por isso, trabalhamos continuamente.</p>
+           </blockquote>
+        </div>
+
+        <div class="box2" data-aos="fade-left">
+            <h2>Atendimento</h2>
+                <p>email: contato@sistemasinsat.com.br</p>
+                 <p>WhatsApp: (11) 2863-0153</p>
+        </div>
+    </section>
+    <section class="section2" id="section2" data-aos="fade-up" data-aos-delay="200">
+        <h1 style="text-align: center; ">Sobre nós</h1>
+<p><br></p>
+<p>A Insat Rastreadores é uma empresa que vem se destacando na área de desenvolvimento e implantação de tecnologias para rastreamento e recuperaçãoe veículos. Investimos em pesquisas e operações de campo, objetivando preencher um espaço vazio e satisfazer as necessidades de um mercado carente e ávido de soluções para os problemas que vem se agravando dia a dia, tais como a segurança pessoal e patrimonial, fundamentalmente no segmento automotivo.</p>
+<p>Atuando na área de assessoria está capacitada a lhe oferecer soluções em sistemas de informações , equipamentos e marketing, assim possibilitando um sucesso garantido a seus negócios.</p>
+<p>Somos a Insat Rastreadores, a mais confiável das empresas de rastreamento via satélite e apresentamos índices de eficiência impressionantes de recuperação de veículos. Somos homologados pela Anatel (Agência Nacional de Telecomunicações).</p>
+<p>Seu veículo monitorado 24 Horas por dia através da nossa central. Seu atendimento é feito por uma equipe treinada e dedicada em todas as operações.</p>
+<br>
+<blockquote style="text-align: center;">Seja muito bem vindo(a)!</blockquote>
+<p>A INSAT SISTEMAS DE RASTREAMENTO está ao seu lado, protegendo você, sua família e seu patrimônio.</p>
+<p>Nossa meta é oferecer o que há de melhor em soluções, produtos e serviços, para atendermos e superarmos às suas expectativas. Este é o nosso compromisso e, por isso, trabalhamos continuamente.</p>
+<p><br></p>
+<p>Buscando sempre a melhoria contínua, estamos a disposição para críticas, sugestões e questionamentos/dúvidas, através do telefone (11) 2863-0153, ou e-mail: atendimento@rastreadoresinsat.com</p>
+<p><br></p><p>A INSAT SISTEMAS DE RASTREAMENTO considera a transparência fundamental no relacionamento com seus clientes, e para isso nos colocamos a disposição para todos esclarecimentos que se fizerem necessários.</p>
+<p><br></p>
+<blockquote style="text-align: center; ">Benefícios</blockquote><p>Os avanços logrados nas áreas das telecomunicações e informática unidas a uma irrevogável vocação de prestadores de serviço, fazem hoje da INSAT um dos destaques no mercado de consumo.</p><p>Os resultados das pesquisas podem ser traduzidos na atualidade em quatro grandes objetivos:</p><p>1. Diminuir as probabilidades de furto ou roubo de veículos;</p><p>2. Aumentar as possibilidades de recuperar veículos e cargas roubada;</p><p>3. Auxiliar na logística do controle de frotas;</p><p>4. Aprimorar os instrumentos de segurança pessoal.</p><p><br></p><p>Empresas de tecnologia e de operação, concessionárias, transportadoras, equipes de negócios, pais de famílias, entre outros, constituem os segmentos que estão formando parcerias com a INSAT, procurando também atender requisitos do mercado na área de segurança pessoal e patrimonial.</p><p><br></p><p class="card-text" style="margin: 0cm; background-image: initial; background-position: initial; background-size: initial; background-repeat: initial; background-attachment: initial; background-origin: initial; background-clip: initial;"><span style="font-family: Arial, sans-serif; color: rgb(33, 37, 41); background-image: initial; background-position: initial; background-size: initial; background-repeat: initial; background-attachment: initial; background-origin: initial; background-clip: initial;">
+    <p>A maturidade tecnológica
+    alcançada ao nível de produtos e serviços pela INSAT, transforma a empresa e
+    seus parceiros de negócios, sejam eles representantes, distribuidores ou
+    franqueados, em um grupo com inquestionáveis vantagens competitivas e alto
+    valor.<o:p></o:p></span></p><p><br></p>
+    </section>
+    <section class="section3" id="section3" data-aos="fade-up" data-aos-delay="400">
+        <h1>Section 3</h1>
+    </section>
+    <section class="section4" id="section4" data-aos="fade-up" data-aos-delay="400">
+        <h1>Section 4</h1>
+        Alguma coisa aqui 
+    </section>
+    <section class="section5" id="section5" data-aos="fade-up" data-aos-delay="400">
+        <h1>Section 5</h1>
+    </section>
+    <section class="section6" id="section6" data-aos="fade-up" data-aos-delay="400">
+        <h1>Section 6</h1>
+    </section>
+    <section class="section7" id="section7" data-aos="fade-up" data-aos-delay="400">
+        <h1>Section 7</h1>
+    </section>
+    <section class="section8" id="section8" data-aos="fade-up" data-aos-delay="400">
+        <h1>Section 8</h1>
+    </section>
+    <section class="section9" id="section9" data-aos="fade-up" data-aos-delay="400">
+        <h1>Section 9</h1>
+    </section>
+    <section class="section10" id="section10" data-aos="fade-up" data-aos-delay="400">
+        <h1>Section 10</h1>
+    </section>
+
+      <!-- Rodapé com a seta -->
+      <footer>
+        <a href="#menu-superior" class="seta-para-cima">&#8679;</a>
+    </footer>
+
+    <script>
+        document.querySelector('.seta-para-cima').addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            const navHeight = document.querySelector('nav').offsetHeight;
+
+            window.scrollTo({
+                top: targetElement.offsetTop - navHeight,
+                behavior: 'smooth'
+            });
+        });
+    </script>
+
+  </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+<script>
+  AOS.init({
+  duration: 1000, // Ajuste a duração da animação conforme necessário
+});
+</script>
+
+<script src="http://unpkg.com/@popperjs/core@2"></script>
+
+    <script>
+        AOS.init({
+            duration: 1200,
+        });
+    </script>
+</body>
+</html>
