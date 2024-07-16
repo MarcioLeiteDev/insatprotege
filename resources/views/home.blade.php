974b006b2b6a166cda9e9030c3288fb6eefe717b
Marcio Leite <marcio.leite@capitalconsig.local>
2024-07-15 23:15:31 -0300

    veiculos e motos front

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,11 +34,11 @@
                     <i class="fa-solid fa-bag-shopping"></i>  Produtos
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#section3"> <i class="fa-solid fa-thumbtack"></i>  Rastreador para Veículos</a></li>
-                  <li><a class="dropdown-item" href="#section4"><i class="fa-solid fa-thumbtack"></i>  Rastreador para Motos</a></li>
-                  <li><a class="dropdown-item" href="#section5"><i class="fa-solid fa-thumbtack"></i>  Rastreador para Utilitários</a></li>
-                  <li><a class="dropdown-item" href="#section6"><i class="fa-solid fa-thumbtack"></i>  Rastreador para Caminhão</a></li>
-                  <li><a class="dropdown-item" href="#section7"><i class="fa-solid fa-thumbtack"></i>  Rastreador para Frotas</a></li>
+                  <li><a class="dropdown-item" href="#section3"> <i class="fa-solid fa-car"></i>  Rastreador para Veículos</a></li>
+                  <li><a class="dropdown-item" href="#section4"> <i class="fa-solid fa-motorcycle"></i>  Rastreador para Motos</a></li>
+                  <li><a class="dropdown-item" href="#section5"><i class="fa-solid fa-truck"></i>  Rastreador para Utilitários</a></li>
+                  <li><a class="dropdown-item" href="#section6"><i class="fa-solid fa-truck-front"></i>  Rastreador para Caminhão</a></li>
+                  <li><a class="dropdown-item" href="#section7"><i class="fa-solid fa-car"></i>  Rastreador para Frotas</a></li>
                 
                 </ul>
               </li>
@@ -78,7 +78,7 @@
 
         <div class="box2" data-aos="fade-left">
             <h2>Atendimento</h2>
-                <p>email: contato@sistemasinsat.com.br</p>
+                <p>email: {{ $email }} </p>
                  <p>WhatsApp: (11) 2863-0153</p>
         </div>
     </section>
@@ -105,11 +105,66 @@
     valor.<o:p></o:p></span></p><p><br></p>
     </section>
     <section class="section3" id="section3" data-aos="fade-up" data-aos-delay="400">
-        <h1>Section 3</h1>
+     <h1>Rastreadores para Carros</h1>
+     <p> Rastreamento via satélite para veículos, localização do veículo via internet ou app, confira abaixo o que nosso rastreador é capaz</p>
+    <div class="Carros_content_box ">
+      <div class="Carrosbox" data-aos="fade-left">
+        <h1><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 320 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M272 0H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h224c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48zM160 480c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32z"></path></svg></h1>
+        <h4>LOCALIZAÇÃO DO VEÍCULO VIA INTERNET OU APP</h4>
+        <p>Você pode localizar seu veículo 24 horas por dia via internet ou celular.</p>
+      </div>
+      <div class="Carrosbox" data-aos="fade-up">
+        <h1><i class="fa-solid fa-globe"></i></h1>
+        <h4>CENTRAL DE MONITORAMENTO 24 HORAS</h4>
+      <p>Em caso de roubo ou localização de seu veículo, você conta com uma central de monitoramento 24 horas por dia, basta ligar em nosso 0800 ligação gratuita, informar a placa de seu veículo e nossa central vai prestar o atendimento necessário para a localização e recuperação do veículo juntamente com a polícia militar local.</p>
+      </div>
+      <div class="Carrosbox" data-aos="fade-down">
+        <h1><i class="fa-solid fa-database"></i></h1>
+        <h4>HISTÓRICO DE POSIÇÕES E VELOCIDADE</h4>
+      <p>A cada 2 minutos o equipamento GPS informa a posição de latitude e longitude para nosso software central, onde ficam armazenadas todas as posições para consultas até os últimos 360 dias, havendo necessidade de históricos mais longos, devesse verificar junto ao nosso setor administrativo a possibilidade de disponibilização dos dados.</p>
+      </div>
+      <div class="Carrosbox" data-aos="fade-right">
+        <h1><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M367.9 329.76c-4.62 5.3-9.78 10.1-15.9 13.65v22.94c66.52 9.34 112 28.05 112 49.65 0 30.93-93.12 56-208 56S48 446.93 48 416c0-21.6 45.48-40.3 112-49.65v-22.94c-6.12-3.55-11.28-8.35-15.9-13.65C58.87 345.34 0 378.05 0 416c0 53.02 114.62 96 256 96s256-42.98 256-96c0-37.95-58.87-70.66-144.1-86.24zM256 128c35.35 0 64-28.65 64-64S291.35 0 256 0s-64 28.65-64 64 28.65 64 64 64zm-64 192v96c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32v-96c17.67 0 32-14.33 32-32v-96c0-26.51-21.49-48-48-48h-11.8c-11.07 5.03-23.26 8-36.2 8s-25.13-2.97-36.2-8H208c-26.51 0-48 21.49-48 48v96c0 17.67 14.33 32 32 32z"></path></svg></h1>
+        <h4>CERCAS ELETRÔNICAS</h4>
+      <p>Você poderá delimitar uma área ou região onde seu veículo opera, ou até se deixar a mesma estacionada, havendo o deslocamento do veículo para outra área você será informado no app e no site de rastreamento imediatamente sobre o deslocamento do veículo.</p>
+      </div>
+    </div>
+    <div class="col-12 text-center whatsapp" data-aos="fade-right">
+      <h3>Duvidas e Orçamentos via Whatsapp</h3>
+     <a href="https://wa.me/{{ $telefone }}" target="_blank" style="text-decoration: none; color:#000;"><h1> <i class="fab fa-whatsapp-square bg-success"></i> Clique Aqui </h1></a> 
+    </div>
+
+    
     </section>
     <section class="section4" id="section4" data-aos="fade-up" data-aos-delay="400">
-        <h1>Section 4</h1>
-        Alguma coisa aqui 
+      <h1>Rastreadores para Motos</h1>
+      <p> Rastreamento via satélite para motos, localização do veículo via internet ou app, confira abaixo o que nosso rastreador é capaz</p>
+     <div class="Carros_content_box ">
+       <div class="Carrosbox" data-aos="fade-left">
+         <h1><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 320 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M272 0H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h224c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48zM160 480c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32z"></path></svg></h1>
+         <h4>LOCALIZAÇÃO DO VEÍCULO VIA INTERNET OU APP</h4>
+         <p>Você pode localizar seu veículo 24 horas por dia via internet ou celular.</p>
+       </div>
+       <div class="Carrosbox" data-aos="fade-up">
+         <h1><i class="fa-solid fa-globe"></i></h1>
+         <h4>CENTRAL DE MONITORAMENTO 24 HORAS</h4>
+       <p>Em caso de roubo ou localização de seu veículo, você conta com uma central de monitoramento 24 horas por dia, basta ligar em nosso 0800 ligação gratuita, informar a placa de seu veículo e nossa central vai prestar o atendimento necessário para a localização e recuperação do veículo juntamente com a polícia militar local.</p>
+       </div>
+       <div class="Carrosbox" data-aos="fade-down">
+         <h1><i class="fa-solid fa-database"></i></h1>
+         <h4>HISTÓRICO DE POSIÇÕES E VELOCIDADE</h4>
+       <p>A cada 2 minutos o equipamento GPS informa a posição de latitude e longitude para nosso software central, onde ficam armazenadas todas as posições para consultas até os últimos 360 dias, havendo necessidade de históricos mais longos, devesse verificar junto ao nosso setor administrativo a possibilidade de disponibilização dos dados.</p>
+       </div>
+       <div class="Carrosbox" data-aos="fade-right">
+         <h1><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M367.9 329.76c-4.62 5.3-9.78 10.1-15.9 13.65v22.94c66.52 9.34 112 28.05 112 49.65 0 30.93-93.12 56-208 56S48 446.93 48 416c0-21.6 45.48-40.3 112-49.65v-22.94c-6.12-3.55-11.28-8.35-15.9-13.65C58.87 345.34 0 378.05 0 416c0 53.02 114.62 96 256 96s256-42.98 256-96c0-37.95-58.87-70.66-144.1-86.24zM256 128c35.35 0 64-28.65 64-64S291.35 0 256 0s-64 28.65-64 64 28.65 64 64 64zm-64 192v96c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32v-96c17.67 0 32-14.33 32-32v-96c0-26.51-21.49-48-48-48h-11.8c-11.07 5.03-23.26 8-36.2 8s-25.13-2.97-36.2-8H208c-26.51 0-48 21.49-48 48v96c0 17.67 14.33 32 32 32z"></path></svg></h1>
+         <h4>CERCAS ELETRÔNICAS</h4>
+       <p>Você poderá delimitar uma área ou região onde seu veículo opera, ou até se deixar a mesma estacionada, havendo o deslocamento do veículo para outra área você será informado no app e no site de rastreamento imediatamente sobre o deslocamento do veículo.</p>
+       </div>
+     </div>
+     <div class="col-12 text-center whatsapp" data-aos="fade-right">
+       <h3>Duvidas e Orçamentos via Whatsapp</h3>
+      <a href="https://wa.me/{{ $telefone }}" target="_blank" style="text-decoration: none; color:#000;"><h1> <i class="fab fa-whatsapp-square bg-success"></i> Clique Aqui </h1></a> 
+     </div>
     </section>
     <section class="section5" id="section5" data-aos="fade-up" data-aos-delay="400">
         <h1>Section 5</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+Route::controller(WebController::class)->prefix('/')->as('./')->group(function(){
+    Route::get('/', 'index')->name('index');
 });
+
+// Route::get('/', function () {
+//     return view('home');
+// });
